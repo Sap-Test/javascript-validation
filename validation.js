ed3dacf6f2d461e34b06193e373d62f86558218e
sapnapawar <sapna.pawar31@gmail.com>
2021-02-11 11:54:11 -0600

    Updated the jQuery

--- validation.js
+++ validation.js
@@ -1,26 +1,60 @@
 $(document).ready(function () {
     // Object containing the validation rules
-    var myRules =
-        {};
+    let myRules =
+        {
+        firstName:
+            {
+                required: true
+            },
+        grade:
+            {
+                required: true,
+                min: 5,
+                max: 8,
+                digits:true
+            }
+
+        };
 
     // Object containing the error messages
-    var myMessages =
-        {};
+    let myMessages =
+        {
+            firstName:
+                {
+                    required: "Please Enter your First Name"
+                },
+            grade:
+                {
+                    required: "Please enter the grade between 5-8",
+                    min: "Please enter more than 4",
+                    max: "Please enter less than 9",
+                    digits: "Please enter number only"
+                }
+
+        };
 
     // Pass the configuration to the form's validate() method
     // Needs submitHandler, rules, and messages properties
     $("form").validate(
-        {}
+        {
+
+            submitHandler: runMyProgram,
+            rules: myRules,
+            message: myMessages
+        }
     );
 
-    function runMyProgram() {
+    function runMyProgram(event) {
         // Change the text of the <p> with ID of "message" to
         // state, for example, "You have registered Jane for grade 6 camp!"
         // Use the name and grade the user provided in the form.
+        event.preventDefault();
+        let firstName = $("input#firstName").val();
 
+        let grade = parseInt($("input#grade").val());
+
+        $("p#message").text(`You have registered ${firstName} for grade ${grade} camp.`)
 
     }
-
-
 
 });
